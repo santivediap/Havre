f1a783fe153966e5872d51b5cd23ae04e6ae0030
santivediap <santivediap@gmail.com>
2026-06-21 16:47:50 +0200

    fix: inline stylesheets, site property

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://havre.santiagovedia.com',
+  build: { inlineStylesheets: 'always' },
   output: 'server',
   adapter: cloudflare(),
   vite: {
